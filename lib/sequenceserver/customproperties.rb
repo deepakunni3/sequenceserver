359fb1a589feb42eb8e655e9a19e8dd09488021a
Deepak Unni <deepak.unni3@gmail.com>
2017-03-02 17:23:09 -0600

    Add BGD specific properties

--- lib/sequenceserver/customproperties.rb
+++ lib/sequenceserver/customproperties.rb
@@ -1,6 +1,8 @@
 module SequenceServer
-  TITLE = 'SequenceServer'
-  BASE_URL = ''
-  APOLLO_WEBAPP_PATH = ''
-  ORGANISMS = {}
+  TITLE = 'BGD BLAST'
+  BASE_URL = 'http://bovinegenome.org'
+  APOLLO_WEBAPP_PATH = 'Apollo2'
+  ORGANISMS = {
+    "UMD3.1" => 22875
+  }
 end
