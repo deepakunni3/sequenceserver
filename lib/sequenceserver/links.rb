c40c12307bdd5e2c3d667fcadcd4ca2d923ebb55
Deepak Unni <deepak.unni3@gmail.com>
2017-03-22 11:19:01 -0500

    Add another case to handle while generating JBrowse links

--- lib/sequenceserver/links.rb
+++ lib/sequenceserver/links.rb
@@ -121,7 +121,7 @@ module SequenceServer
         url = ''
       elsif
         representative_database = whichdb[0].name
-        organism_prefix = representative_database.split('/')[-1].split('-')[0]
+        organism_prefix = representative_database.split('/')[-1].split('_')[2]
         organism_id = 0
 
         if SequenceServer::ORGANISMS.has_key?(organism_prefix)
@@ -154,6 +154,15 @@ module SequenceServer
           end
 
           url = "#{SequenceServer::BASE_URL}/#{SequenceServer::APOLLO_WEBAPP_PATH}/#{organism_id}/jbrowse/index.html?loc=#{feature_id}"
+        else
+          # SS parses the seqids and is of the format:
+          # UMD3.1:SEQNAME
+          seq_name = id.split(':')[1]
+          fmin = hsps.map(&:sstart).min
+          fmax = fmin + hsps.map(&:length).max
+          location_string = "#{seq_name}:#{fmin}..#{fmax}"
+          location_string = encode location_string
+          url = "#{SequenceServer::BASE_URL}/#{SequenceServer::APOLLO_WEBAPP_PATH}/#{organism_id}/jbrowse/index.html?loc=#{location_string}&highlight=#{location_string}"
         end
       end
 
