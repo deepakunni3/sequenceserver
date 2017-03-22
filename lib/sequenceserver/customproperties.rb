096115860d2727e6c1363063edd72cc7a878236a
Deepak Unni <deepak.unni3@gmail.com>
2017-03-22 13:48:31 -0500

    Add Btau_4.6.1 to custom properties

--- lib/sequenceserver/customproperties.rb
+++ lib/sequenceserver/customproperties.rb
@@ -3,6 +3,7 @@ module SequenceServer
   BASE_URL = 'http://bovinegenome.org'
   APOLLO_WEBAPP_PATH = 'Apollo2'
   ORGANISMS = {
-    "UMD3.1" => 22875
+    "UMD3.1" => 22875,
+    "Btau_4.6.1" => 2784249
   }
 end
